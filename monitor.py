5ca6a614b7d8410882d26d6b99fb7b50eb953cb4
Jebbs <masomc01.s@gmail.com>
2019-02-05 09:38:35 +0000

    Fixed whitespace

--- monitor.py
+++ monitor.py
@@ -4,10 +4,11 @@ from datetime import datetime
 sys_uptime = "uptime | awk -F',' '{print $1}'"
 load_average = "uptime | awk -F',' '{print $4 $5 $6}'"
 disk_used = "/bin/df -h | grep -vE 'Filesystem|tmpfs'"
-mem_used  = "free -m"
+mem_used = "free -m"
 check_services = "service --status-all"
 
 os.system(sys_uptime)
 os.system(load_average)
 os.system(disk_used)
 os.system(mem_used)
+
