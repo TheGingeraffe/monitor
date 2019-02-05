2fe16dc4566ee3d38da6db5e0e70c59f739d9cc2
Jebbs <masomc01.s@gmail.com>
2019-02-05 09:34:34 +0000

    initial monitor for core resource check

---
+++ monitor.py
@@ -0,0 +1,13 @@
+import os
+from datetime import datetime
+
+sys_uptime = "uptime | awk -F',' '{print $1}'"
+load_average = "uptime | awk -F',' '{print $4 $5 $6}'"
+disk_used = "/bin/df -h | grep -vE 'Filesystem|tmpfs'"
+mem_used  = "free -m"
+check_services = "service --status-all"
+
+os.system(sys_uptime)
+os.system(load_average)
+os.system(disk_used)
+os.system(mem_used)
