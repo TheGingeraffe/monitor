91ff396eb7c80f203de1ba4fc5b6844af0e0fd48
Jebbs <masomc01.s@gmail.com>
2019-02-07 13:39:09 +0000

    replaced system call with subprocess for better formatting on output.

--- monitor.py
+++ monitor.py
@@ -1,14 +1,21 @@
 import os
-from datetime import datetime
+import subprocess
 
 sys_uptime = "uptime | awk -F',' '{print $1}'"
-load_average = "uptime | awk -F',' '{print $4 $5 $6}'"
+load_average = "uptime | awk -F\"[^0-9]:\" '{print $2}'"
 disk_used = "/bin/df -h | grep -vE 'Filesystem|tmpfs'"
 mem_used = "free -m"
-check_services = "service --status-all"
 
-os.system(sys_uptime)
-os.system(load_average)
-os.system(disk_used)
-os.system(mem_used)
-
+Uptime = subprocess.Popen(sys_uptime,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+Load = subprocess.Popen(load_average,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+Disk = subprocess.Popen(disk_used,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+Mem = subprocess.Popen(mem_used,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
+uptime_output = Uptime.communicate()[0]
+load_output = Load.communicate()[0]
+disk_output = Disk.communicate()[0]
+mem_output = Mem.communicate()[0]
+print("Uptime: {}".format(bytes.decode(uptime_output)))
+print("Load Average: {}".format(bytes.decode(load_output)))
+print("Disk Used:\n {}".format(bytes.decode(disk_output)))
+print("Memory Used:\n {}".format(bytes.decode(mem_output)))
+ 
